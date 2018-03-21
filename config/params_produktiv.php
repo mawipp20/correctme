ccbe2bfc4c8378af3d7e9cbf25157f8418a01c60
Martin Wippersteg <wippersteg@zweiundzwei.de>
2018-03-21 09:50:03 +0100

    21.03.2018

---
+++ config/params_produktiv.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'correctmeHasPoll' => true,
+    'correctmeHasLesson' => false,
+    'restcorrectmeBaseUrl' => 'https://correctme.de/restcorrectme/',
+    'restcorrectmePath' => 'restcorrectme/',
+];
